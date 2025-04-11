86965bf6be5659b420a146988ee1017c5548ca19
GaurangSingh007 <108506613+GaurangSingh007@users.noreply.github.com>
2025-04-11 11:54:21 +0530

    Automated upload process of any changes made in resume done locally

--- infra/main.tf
+++ infra/main.tf
@@ -27,41 +27,38 @@ resource "aws_iam_role" "iam_for_lambda" {
 EOF
 }
 
-resource "aws_iam_policy" "iam_policy_for_resume_project" {
-
+resource "aws_iam_policy" "resume_project_policy" {
   name        = "aws_iam_policy_for_terraform_resume_project_policy"
   path        = "/"
   description = "AWS IAM Policy for managing the resume project role"
-    policy = jsonencode(
-    {
-      "Version" : "2012-10-17",
-      "Statement" : [
-        {
-          "Action" : [
-            "logs:CreateLogGroup",
-            "logs:CreateLogStream",
-            "logs:PutLogEvents"
-          ],
-          "Resource" : "arn:aws:logs:*:*:*",
-          "Effect" : "Allow"
-        },
-        {
-          "Effect" : "Allow",
-          "Action" : [
-            "dynamodb:UpdateItem",
-			      "dynamodb:GetItem",
-            "dynamodb:PutItem"
-          ],
-          "Resource" : "arn:aws:dynamodb:*:*:table/cloud-resume"
-        },
-      ]
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Action" : [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        "Resource" : "arn:aws:logs:*:*:*",
+        "Effect" : "Allow"
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "dynamodb:UpdateItem",
+          "dynamodb:GetItem",
+          "dynamodb:PutItem"
+        ],
+        "Resource" : "arn:aws:dynamodb:*:*:table/cloud-resume"
+      }
+    ]
   })
 }
 
 resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
-  role = aws_iam_role.iam_for_lambda.name
-  policy_arn = aws_iam_policy.iam_policy_for_resume_project.arn
-  
+  role       = aws_iam_role.iam_for_lambda.name
+  policy_arn = aws_iam_policy.resume_project_policy.arn
 }
 
 data "archive_file" "zip_the_python_code" {
@@ -70,7 +67,7 @@ data "archive_file" "zip_the_python_code" {
   output_path = "${path.module}/lambda/func.zip"
 }
 
-resource "aws_lambda_function_url" "url1" {
+resource "aws_lambda_function_url" "myfunc_url" {
   function_name      = aws_lambda_function.myfunc.function_name
   authorization_type = "NONE"
 
@@ -83,3 +80,46 @@ resource "aws_lambda_function_url" "url1" {
     max_age           = 86400
   }
 }
+
+# Import an existing S3 bucket
+resource "aws_s3_bucket" "frontend" {
+  bucket         = "gaurang-cloud-resume-challenge"
+  force_destroy  = true
+}
+
+# Allow public access (optional - needed if using public-read)
+resource "aws_s3_bucket_public_access_block" "frontend_block" {
+  bucket = aws_s3_bucket.frontend.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+# Allow public read access to all objects in the bucket
+resource "aws_s3_bucket_policy" "frontend_policy" {
+  bucket = aws_s3_bucket.frontend.id
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject",
+        Effect    = "Allow",
+        Principal = "*",
+        Action    = "s3:GetObject",
+        Resource  = "arn:aws:s3:::gaurang-cloud-resume-challenge/*"
+      }
+    ]
+  })
+}
+
+# Upload local index.html to the bucket
+resource "aws_s3_object" "index_html" {
+  bucket       = aws_s3_bucket.frontend.id
+  key          = "index.html"
+  source       = "C:/Users/gaura/Documents/Gaurang static/cloud-resume-challenge/resume/index.html"
+  content_type = "text/html"
+ etag = filemd5("C:/Users/gaura/Documents/Gaurang static/cloud-resume-challenge/resume/index.html")
+}
+
